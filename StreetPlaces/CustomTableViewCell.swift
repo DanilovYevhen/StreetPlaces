b8c255082541bffc538a17f6abcde35c6ee90258
Yevhen Danilov <danilov.swift@gmail.com>
2022-12-02 15:56:16 +0200

    Create a new class CustomTableViewCell, set up a cell, change MainViewController

---
+++ StreetPlaces/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  StreetPlaces
+//
+//  Created by Yevhen Danilov on 02.12.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+}
